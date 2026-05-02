faeced8d99050cfe89ce37cb6150db4ee780e2fd
ktysu1026 <145572360+ktysu1026@users.noreply.github.com>
2026-05-02 14:55:43 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -23,7 +23,7 @@ def img2text(url):
     return text
 
 # Main part
-st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
+st.set_page_config(page_title="Story on Christmas Eve", page_icon="🦜")
 st.header("Turn Your Image to Audio Story")
 uploaded_file = st.file_uploader("Select an Image...")
 
