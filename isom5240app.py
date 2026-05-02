f01e571ed2affeef509ebf6c6c98c7af9c31414b
ktysu1026 <145572360+ktysu1026@users.noreply.github.com>
2026-05-02 17:01:44 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -19,7 +19,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Open and display the image
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption=""Uploaded Image"", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     with st.spinner("Classifying..."):
         # Classify age
